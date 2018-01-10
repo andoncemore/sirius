34a5424e45070cf67076bded82940b9a29a7192c
Joe Rickerby <joerick@mac.com>
2018-01-10 19:10:43 +0000

    Support unicode html

--- sirius/coding/image_encoding.py
+++ sirius/coding/image_encoding.py
@@ -117,7 +117,10 @@ def html_to_png(html):
         # note that the .html suffix is required to make phantomjs
         # pick up the mime-type and render correctly.
         with tempfile.NamedTemporaryFile(suffix='.html') as f:
-            f.write(html)
+            if isinstance(html, unicode):
+                f.write(html.encode('utf8'))
+            else:
+                f.write(html)
             f.flush()
             driver.get('file://' + f.name)
             data = io.BytesIO(driver.get_screenshot_as_png())
